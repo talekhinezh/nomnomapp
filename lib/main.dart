980b52fa9176153177ba5a869cb2f948af6209d3
Joshua Primero <jprimero@gmail.com>
2020-05-18 03:47:11 -0400

    Add transactions page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:nomnom/transactions.dart';
+import 'items.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,196 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food Items',
-      home: MyHomePage(),
+      initialRoute: '/transactions',
+      routes: {
+        '/items': (content) => FoodItemsPage(),
+        '/transactions': (context) => TransactionsPage()
+      }
     );
   }
 }
 
-// Create a Form widget.
-class AddItemForm extends StatefulWidget {
-  @override
-  AddItemFormState createState() {
-    return AddItemFormState();
-  }
-}
 
-// Create a corresponding State class.
-// This class holds data related to the form.
-class AddItemFormState extends State<AddItemForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
-  final _formKey = GlobalKey<FormState>();
-  final nameController = TextEditingController();
-  final costController = TextEditingController();
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    nameController.dispose();
-    costController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            controller: nameController,
-            decoration: InputDecoration(
-                labelText: "Enter name of item"
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: costController,
-            decoration: InputDecoration(
-                labelText: "Enter PHP cost of item"
-            ),
-            keyboardType: TextInputType.number,
-            inputFormatters: [
-              WhitelistingTextInputFormatter.digitsOnly
-            ],
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter a number';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false
-                // otherwise.
-                if (_formKey.currentState.validate()) {
-                  // If the form is valid, display a Snackbar.
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Saving...')));
-                  Firestore.instance.collection('items')
-                    .document()
-                    .setData({
-                      'name': nameController.text,
-                      'cost': int.parse(costController.text),
-                    }).then((value) => Navigator.pop(context));
-                }
-              },
-              child: Text('Save'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AddItemPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Add New Item"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-        child: AddItemForm()
-      )
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Food Items')),
-      body: _buildBody(context),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddItemPage()),
-          );
-        },
-        tooltip: 'Add Item',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  Widget _buildBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('items').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
-
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.cost.toString()),
-          onTap: () => print(record),
-        ),
-      ),
-    );
-  }
-}
-
-class Record {
-  final String name;
-  final int cost;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['name'] != null),
-        name = map['name'],
-        cost = map['cost'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-  @override
-  String toString() => "Record<$name:$cost>";
-}
---
+++ lib/transactions.dart
@@ -0,0 +1,103 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class TransactionsPage extends StatefulWidget {
+  @override
+  _TransactionsPageState createState() {
+    return _TransactionsPageState();
+  }
+}
+
+class _TransactionsPageState extends State<TransactionsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Transactions')),
+      drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                  title: Text('Transactions'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/transactions');
+                  }
+              ),
+              ListTile(
+                  title: Text('Items'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/items');
+                  }
+              ),
+            ],
+          )
+      ),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('transactions').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 19.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final transaction = Transaction.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(transaction.transactionId),
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: ListTile(
+          title: Text("PHP" + transaction.totalCost.toString()),
+          subtitle: Text(transaction.items),
+          trailing: Text(timeago.format(transaction.timestamp.toDate())),
+        ),
+      ),
+    );
+  }
+}
+
+class Transaction {
+  final String transactionId;
+  final int totalCost;
+  final Timestamp timestamp;
+  final DocumentReference reference;
+  final String items;
+
+  Transaction.fromMap(Map<String, dynamic> map, {this.reference})
+      : this.transactionId = reference.documentID,
+        totalCost = List.from(map['items'])
+            .map((e) => Map.from(e)['cost'] * Map.from(e)['amount'])
+            .reduce((value, element) => value + element),
+        timestamp = map['timestamp'],
+        items = List.from(map['items']).map((e) =>
+          Map.from(e)['name'] + ' x' + Map.from(e)['amount'].toString()
+        ).join(", ")
+  ;
+
+  Transaction.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference)
+  ;
+
+  @override
+  String toString() => "Record<$timestamp>";
+}
