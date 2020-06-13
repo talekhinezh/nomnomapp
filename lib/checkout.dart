afeea2602fb2016215e5151723568e6ecdad7784
Joshua Primero <jprimero@gmail.com>
2020-06-13 11:44:42 -0400

    Fix UI

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -11,21 +11,7 @@ class CheckoutPage extends StatefulWidget {
 }
 
 class _CheckoutPageState extends State<CheckoutPage> {
-  int _selectedIndex = 0;
-  String category = 'food';
   Map<CheckoutItem, int> items = {};
-
-  void _onItemTapped(int index) {
-    setState(() {
-      switch(index) {
-        case 0: category = 'food'; break;
-        case 1: category = 'drinks'; break;
-        case 2: category = 'burgers'; break;
-      }
-      _selectedIndex = index;
-    });
-  }
-
   String _total() {
     return '₱' + items.entries.map(
             (e) => (e.key.record.cost + e.key.extras.map((e) => e.cost).fold(0, (a, b) => a + b)) * e.value)
@@ -35,75 +21,105 @@ class _CheckoutPageState extends State<CheckoutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Checkout')),
+      appBar: AppBar(
+        title: Text('Checkout')
+      ),
       drawer: NomNomDrawer(),
-      body: _buildBody(context),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        icon: const Icon(Icons.check),
-        label: Text('Checkout - ' + _total()),
-        backgroundColor: Colors.green,
-        onPressed: () {
-          Firestore.instance.collection('orders')
-              .add({
-                'timestamp': DateTime.now(),
-                'items': items.entries.map((e) => {
-                  'name': e.key.record.name,
-                  'cost': e.key.record.cost,
-                  'itemId': e.key.record.id,
-                  'extras': e.key.extras.map((extra) => {'name': extra.name, 'cost': extra.cost}).toList(),
-                  'amount': e.value,
-                }).toList()
-              })
-            .then((value) => Navigator.pushReplacementNamed(context, '/orders'));
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.restaurant),
-            title: Text("Food")
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            child: RaisedButton(
+              elevation: 10.0,
+              textColor: Colors.white,
+              padding: const EdgeInsets.fromLTRB(35.0, 15.0, 35.0, 15.0),
+              color: Colors.green,
+              child: Text(
+                  'Checkout - ' + _total(),
+                  style: TextStyle(fontSize: 20)
+              ),
+              onPressed: () {
+                Firestore.instance.collection('orders')
+                    .add({
+                  'timestamp': DateTime.now(),
+                  'items': items.entries.map((e) => {
+                    'name': e.key.record.name,
+                    'cost': e.key.record.cost,
+                    'itemId': e.key.record.id,
+                    'extras': e.key.extras.map((extra) => {'name': extra.name, 'cost': extra.cost}).toList(),
+                    'amount': e.value,
+                  }).toList()
+                })
+                .then((value) => Navigator.pushReplacementNamed(context, '/orders'));
+              },
+            ),
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.free_breakfast),
-            title: Text("Drinks")
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood),
-            title: Text("Burgers"),
-            backgroundColor: Colors.green
-          ),
+          Expanded(
+            child: DefaultTabController(
+              length: 3,
+              child:
+                Column(
+                  children: [
+                      Expanded(
+                          child: TabBarView(
+                            children: [
+                              _buildBody(context, 'food'),
+                              _buildBody(context, 'drinks'),
+                              _buildBody(context, 'burgers'),
+                            ].toList(),
+                          )
+                      ),
+                      Container(
+                          color: Colors.blue,
+                          child: TabBar(
+                              indicatorColor: Colors.white,
+                              tabs: [
+                                Tab(
+                                  text: 'Food',
+                                  icon: Icon(Icons.restaurant),
+                                ),
+                                Tab(
+                                  text: 'Drinks',
+                                  icon: Icon(Icons.free_breakfast),
+                                ),
+                                Tab(
+                                  text: 'Burgers',
+                                  icon: Icon(Icons.fastfood),
+                                ),
+                              ].toList()
+                          )
+                      ),
+                    ]
+                )
+            )
+          )
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped
-      ),
+      ),//_buildBody(context),
     );
   }
 
-  Widget _buildBody(BuildContext context) {
+  Widget _buildBody(BuildContext context, String category) {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('items').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
-        return _buildList(context, snapshot.data.documents);
+        return _buildList(context, snapshot.data.documents, category);
       },
     );
   }
 
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot, String category) {
     return Center(
       child: Container(
           constraints: BoxConstraints(maxWidth: 500),
           child: ListView(
-            padding: const EdgeInsets.only(top: 19.0),
-            children: snapshot
-                .map((e) => Record.fromSnapshot(e))
-                .where((record) => record.categories.contains(category))
-                .map((record) => _buildListItem(context, record))
-                .toList(),
-          )
+              children: snapshot
+                  .map((e) => Record.fromSnapshot(e))
+                  .where((record) => record.categories.contains(category))
+                  .map((record) => _buildListItem(context, record))
+                  .toList(),
+            )
       )
     );
   }
